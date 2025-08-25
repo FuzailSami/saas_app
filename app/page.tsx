c61222581cb21131e335a7a3f959fd684d70d09e
Fuzail Sami <samifuzail@gmail.com>
2025-08-25 12:25:48 -0500

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,45 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
-
+import CTA from '@/components/CTA'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import { recentSessions } from '@/constants'
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my Saas App</h1>
-      <Button className='mt-4'> Let's get started </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+      <section className='home-section'> 
+        <CompanionCard
+        id="123"
+        name="Neura the Brainy Expolerer"
+        topic="Neural Netwrok of the Brain"
+        subject="Science"
+        duration={45}
+        color="#ffda6e"
+        />
+        <CompanionCard
+        id="456"
+        name="Countsy the Number Wizard"
+        topic="Derivatives and Integrals"
+        subject="Maths"
+        duration={30}
+        color="#e5d0ff"/>
+        <CompanionCard
+        id="123"
+        name="Verba the Vocabulary Builder"
+        topic="English Literature"
+        subject="Language"
+        duration={30}
+        color="#ffda6e"/>
+       </section>
+       <section className='home-section'>
+        <CompanionsList
+        title="Recently completed sessions"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg:w-full"
+        
+        />
+        <CTA/>
+       </section>
+    </main>
   )
 }
 
---
+++ components/CTA.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+const CTA = () => {
+  return (
+    <section className='cta-section'>
+      <div className='cta-badge'> Start learning your way.</div>
+      <h2 className='text-3xl font-bold'>
+        Build and Personalize Learning Companion
+      </h2>
+      <p>Pick a name, subject, voice, & personality - and 
+        start learning through voice conversations that feel natural</p>
+      <Image src="/images/cta.svg" alt="cta" width={362} height={232} />
+      <Link href="/companions/new">
+        <button className='btn-primary'>
+          <Image src="/icons/plus.svg" alt="plus" width={12} height={12} />
+          <span>Build a New Companion</span>
+        </button>
+      </Link>
+    </section>
+
+  )
+}
+
+export default CTA
+
